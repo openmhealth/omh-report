03bf4b6c7ed2ce3f6fce317fd3e82469500e1123
Jeroen Ooms <jeroen.ooms@stat.ucla.edu>
2013-12-19 13:07:52 -0800

    Update omh-report.js

--- omh-report.js
+++ omh-report.js
@@ -2,17 +2,13 @@
 
 $(document).ready(function() {
 
-	opencpu.seturl("//public.opencpu.org/ocpu/library/dpu.mobility/R")
+	opencpu.seturl("//test.ohmage.org/ocpu/library/dpu.mobility/R")
 	
 	//globalz
 	var n = 14;
 	var today = new Date();
 	var startdate = new Date(today.getFullYear(), today.getMonth(), today.getDate()-n);
-	
-	//we match the protocol (http vs https) with the current server. IE requires that for CORS.
-	//public.opencpu.org should support either.
-	var opencpuserver = window.location.protocol + "//public.opencpu.org"
-	
+
 	//crossfilter data
 	var mobility;
 
